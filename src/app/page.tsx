6ed254c18b22792da03e9fb28dbbf9d7c6091e00
Rizkieka546 <reka48644@gmail.com>
2025-12-22 17:00:59 +0700

    perbaikan dikit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,10 @@ export default function Page() {
   const { user } = useAuthStore();
 
   useEffect(() => {
-    if (!user) return;
+    if (!user) {
+      router.replace("/login");
+      return;
+    }
 
     if (user.role === "admin") {
       router.replace("/admin");
